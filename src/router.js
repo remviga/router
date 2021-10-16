778acb5cae60cd2acd60de2a040d4fa9595b6fe6
Viguen <viguen@MacBook-Pro-Vigen.local>
2021-10-16 22:40:04 +0300

    Class implementation added

--- src/router.js
+++ src/router.js
@@ -47,7 +47,7 @@ const BrowserRouter = function ({ routes, viewContainer }) {
 
   /** */
   window.addEventListener("popstate", renderRouteByPathname);
-  
+
   /** */
   Array.from(document.querySelectorAll("a")).forEach((link) =>
     link.addEventListener("click", linkClickHandler)
@@ -59,3 +59,63 @@ const BrowserRouter = function ({ routes, viewContainer }) {
     renderRoute,
   };
 };
+
+/** Class implementation variant */
+class BrowserRouterClass {
+  constructor(options) {
+    const browserOptions = options || {};
+
+    const { routes, viewContainer } = browserOptions;
+
+    this.routesList = routes || [];
+    this.container = viewContainer;
+
+    this.setup();
+  }
+  /** */
+  renderRoute(route) {
+    if (!route) throw new Error("route is not present");
+
+    if (!Array.from(this.container.childNodes).length) {
+      this.container.appendChild(route.component);
+    } else {
+      this.container.replaceChild(route.component, this.container.firstChild);
+    }
+  }
+  /** */
+  renderRouteByPathname() {
+    const route = this.routesList.find((r) => r.path === location.pathname);
+    this.renderRoute(route);
+  }
+  /** */
+  addRoute(route) {
+    this.routesList.push(route);
+
+    this.renderRouteByPathname();
+
+    return route;
+  }
+  /** */
+  getRoutes = () => {
+    return this.routesList;
+  };
+  /** */
+  linkClickHandler = (e) => {
+    e.preventDefault();
+
+    const url = new URL(e.currentTarget.href);
+    const route = this.routesList.find((r) => r.path === url.pathname);
+
+    history.pushState({}, document.title, url.pathname);
+
+    this.renderRoute(route);
+  };
+  setup() {
+    /** */
+    window.addEventListener("popstate", () => this.renderRouteByPathname());
+    /** */
+    Array.from(document.querySelectorAll("a")).forEach((link) =>
+      link.addEventListener("click", this.linkClickHandler)
+    );
+  }
+}
